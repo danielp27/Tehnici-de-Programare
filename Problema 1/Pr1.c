ca153e600692cddeba7a342e87b852f91762d76c
danielp27 <daniel.pascu@student.upt.ro>
2022-04-11 13:02:36 +0300

    auto push 3

---
+++ Problema 1/Pr1.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned int oglindire_biti(unsigned int x){
+	unsigned int y = 0;
+
+	while(x > 0) {
+		y <<= 1;
+		if(x & (1 == 1))
+			y ^=1;
+		x >>= 1;
+	}
+
+	return y;
+}
+
+int main() {
+	unsigned int x;
+	printf("Introduceti un numar intreg x: ");
+	scanf("%d", &x);
+	printf("y = %d", oglindire_biti(x));
+	return 0;
+}
