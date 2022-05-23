22d047b7c70d519837aa1ad5c1c3876a1f6f7491
danielp27 <daniel.pascu@student.upt.ro>
2022-05-23 12:50:25 +0300

    auto push 3

--- Problema/Test_2.c
+++ Problema/Test_2.c
@@ -1,7 +1,22 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+int xor(int v[100], int n) {
+
+}
+
 int main() {
+	int n, i, v[100];
+	FILE *f;
+
+	printf("Introduceti numarul de elemente din vector: ");
+	scanf("%d", &n);
+
+	printf("Introduceti elementele vectorului:\n");
+	for (i = 0; i < n; i++) {
+		printf("v[%d] = ", i);
+		scanf("%d", &v[i]);
+	}
 
 	return 0;
 }
